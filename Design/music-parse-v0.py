406c850c2dd7aad45cc3d8417f832b5af80d266e
ntdragon <birdsall_99@comcast.net>
2017-11-02 14:31:46 -0400

    Changes and adds to get initial python running

--- Design/music-parse-v0.py
+++ Design/music-parse-v0.py
@@ -12,7 +12,7 @@ Assumptions and defaults:
 	npm-list2, npm-list3 and npm-list4 to be open for write to eliminate prior entries in them
 	npm-list2.txt will be located songs - if both songbooks are used then 2a and 2b will be used
 	npm-list3.txt will be other songs
-	npm-list4.txt will be the listing containing npm-list2.txt and npm-list3.txt 
+	npm-list4.txt will be the listing containing npm-list2.txt and npm-list3.txt
 	GatherSearch.txt and Gather3Search.txt will be the search files for Gather Comprehensive and Gather 3 respectively
 	GatherPrint.txt and Gather3Print.txt will be the print files for Gather Comprehensive and Gather 3 respectively
 
@@ -35,61 +35,145 @@ Process
 #=======================================================================================================================
 # Imports here
 # ==============================
+from time import sleep
+# =========================================
+# =====     Global Constants Here     =====
+# =========================================
+GREETINGS = """ Greetings
+====================
+     Welcome to Music Suggestions Version 0.1
+     This program is to assist you in setting up your songs for an event at church with suggestions you
+     gather from NPM or other sources.
+     Initially you will be asked for the Liturgical Year and what event (like First Sunday of Advent)
+     and then for the suggestions in the same format as the NPM webpage.
+"""
 
 # Now function definitions
 # ==============================
 def findTitle(the_line, Cpos, Song_Title):
-
+     pass
 def findSong(the_line, Cpos, SongBook, Song_Book, Song_Number):
-
+     pass
 def setOutputLine(Line_Number, Song_Title, Song_Book, Song_Number, Lyne_Out):
-    Lyne_Out = repr(Line_Number) + '\t' + repr(Song_Book, Song_Number) + '/t' +repr(Song_Title)  
+     pass
+#    Lyne_Out = repr(Line_Number) + '\t' + repr(Song_Book, Song_Number) + '/t' +repr(Song_Title)
 
+# ======================================================================================================================
+# =========================================
+# =====     User I/O General Area     =====
+# =========================================
+
+def Greet_User():
+     """
+     Just a display with opening greeting and explanation of process
+     """
+     print(GREETINGS)
+     sleep(10)
+
+def Which_Event():
+     """
+     Initially just ask for the Liturgical Year and event but later will be a selector
+     """
+     pass
+def Which_Songbook():
+     """
+     Initially ask but later look in profile.  Initial options are GC or G3 or both
+     """
+     pass
+def Input_Arrays():
+     """
+     Input Print and Search Arrays for GC and G3
+     """
+     pass
+# =========================================
+# =====     User I/O General Area     =====
+# =========================================
+# ======================================================================================================================
+# ========================================
+# =====     NPM Suggestions Area     =====
+# ========================================
+def Input_NPM_Suggestions():
+     """
+     Set up a text box for the user to paste suggestions.  There may be sections (groupings of songs for various
+     purposes).  These groupings will always have Psalm and Songs but may have others.  ?? ID by starting with a
+     character (??)??  Save in a line by line array?
+     """
+     pass
+def Search_NPM_Suggestions():
+     """
+     Look through suggestions (first by title and then grep for songbook) and sort out known for the songbook
+     If for both then GC first then G3
+     ----------------------------------------
+     For each songbook
+     	for current line in npm suggestion array
+          		if start of section then move to npm display array in both known and unknown areas else
+     		try to see if current line starts with known suggestion
+     			if known then look up and enter known(s) into npm display array
+     				else loop until found or end, at end put into npm unknown part of display
+     	loop till end of suggestion array
+     next songbook or end
+     """
+     """while line in InFyleLine :
+          Line_Number = Line_Number +1
+          the_line = line.readline()
+          findTitle
+          findSong
+          setOutputLine
+          OutFyleLine.write(Lyne_Out)
+     """
+     pass
+def Display_NPM_Suggestions():
+     """
+     Display a mutable view of NPM Suggestions; first known then unknown.  Allow user to delete false positives and
+     change unkown to a known format.  Allow for sections in display
+     """
+     pass
+def Fyle_Output_NPM_Suggestions():
+     """
+     Save to file formatted NPM Suggestions
+     """
+     pass
+def Print_Output_NPM_Suggestions():
+     """
+     Print out formatted NPM Suggestions by song book
+     """
+     pass
+# ========================================
+# =====     NPM Suggestions Area     =====
+# ========================================
 
 #A few variable definitions here
-Input_Fyle1 = 'npm-1.txt'
-Output_GC_Fyle = 'npm-list2a.txt'
-Output_G3_Fyle = 'npm-list2b.txt'
-Output_un_Fyle = 'npm-list3.txt'
-Final_Output_Fyle = 'npm-list4.txt'
-Song_Title = ''
-Song_Book = ''
-Song_Number = ''
-Line_Number = 0
-Num_Songbooks = 1
-SongBook_1 = 'GC '
-SongBook_2 = 'G3 '
-InFyleLine = open(Input_Fyle,'r')
-OutFyleLine = open(Output_Fyle,'w')
+#Input_Fyle1 = 'npm-1.txt'
+#Output_GC_Fyle = 'npm-list2a.txt'
+#Output_G3_Fyle = 'npm-list2b.txt'
+#Output_un_Fyle = 'npm-list3.txt'
+#Final_Output_Fyle = 'npm-list4.txt'
+#Song_Title = ''
+#Song_Book = ''
+#Song_Number = ''
+#Line_Number = 0
+#Num_Songbooks = 1
+#SongBook_1 = 'GC '
+#SongBook_2 = 'G3 '
+#InFyleLine = open(Input_Fyle, 'r')
+#OutFyleLine = open(Output_Fyle, 'w')
 
 
 
-# ==============================
-# =====  MAIN Starts Here  =====
-# ==============================
-
-Greet_User
-Which_Event
-Which_Songbook
-Input_Arrays
-
-# Main loop
-# ---------
-while line in InFyleLine :
-    Line_Number = Line_Number +1
-    the_line = line.readline()
-    findTitle
-    findSong
-    setOutputLine
-    OutFyleLine.write(Lyne_Out)
-
-# Set up and display npm-list4
-# ----------------------------
-Set_List4
-Display_List4
+# ==================================================================================================
+# =====     MAIN Starts Here     =====     MAIN Starts Here     =====     MAIN Starts Here     =====
+# ==================================================================================================
+Greet_User()
+# Which_Event()
+# Which_Songbook()
+# Input_Arrays()
+# Input_NPM_Suggestions()
+# Search_NPM_Suggestions()
+# Display_NPM_Suggestions()
+# Fyle_Output_NPM_Suggestions()
 
 # All Done - Goodbye
-# =================== 
+# ===================
 
 
 #=======================================================================================================================
