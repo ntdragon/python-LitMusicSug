5f5c9a2a06fcf369de6bd556dc28c3bd41c10f77
MikeBirdsall <mgbirdsall@gmail.com>
2017-12-10 10:44:48 -0500

    Add program to convert songs into database records

---
+++ src/conversion/convertsongs.py
@@ -0,0 +1,65 @@
+""" Convert Song Table - Convert songbook text files into database records """
+import argparse
+import sys
+import sqlite3
+
+def convert(book, infile, db):
+    """ Do the conversion for a file """
+
+    conn = sqlite3.connect(db)
+
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS Songs
+        (id integer primary key autoincrement not null,
+         book text,
+         number integer,
+         type text,
+         title text)''')
+
+
+    for line in infile:
+        num, title = line.split("|")
+
+        num = num.strip()
+        inum = int(num)
+        title = title.strip()
+
+        # Special sentinel values at start and end are not actual songs
+        # Perhaps this should be specific to first and last entry
+        # but to do so I would have to structure to know if last entry
+        # before adding
+        if all([v == '0' for v in num]):
+            continue
+        if all([v == '9' for v in num]) and title.startswith("End of Song"):
+            continue
+
+        # Very simple rule for determining Psalm - check with Ed.
+        btype = "Psalm" if title.startswith("Psalm") else "Song"
+
+        # Could gather all entries and use an executemany
+        # which would solve knowing which entry is last
+        entry = (book, inum, btype, title)
+        c.execute("INSERT INTO Songs (book, number, type, title) VALUES (?,?,?,?)",
+            entry)
+
+    conn.commit()
+    conn.close()
+
+def main():
+    """ Run as command line program """
+
+    parser = argparse.ArgumentParser(
+        description="Load database song table from infile")
+    parser.add_argument("book", help="Identifier for songbook")
+    parser.add_argument("infile", nargs='?', type=argparse.FileType('r'),
+        default=sys.stdin, help="Input file (stdin used if left blank)")
+    parser.add_argument("database", help="Database created or updated")
+
+    args = parser.parse_args()
+
+    convert(args.book, args.infile, args.database)
+
+
+if __name__ == "__main__":
+    main()
+
