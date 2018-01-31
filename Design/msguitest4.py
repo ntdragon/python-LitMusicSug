73b78779b179eba12948252e6c7b347c981bdde8
ntdragon <birdsall_99@comcast.net>
2018-01-31 14:32:08 -0500

    Got the GUI to work as far as display.  Now for contents and funtionality

--- Design/msguitest4.py
+++ Design/msguitest4.py
@@ -21,7 +21,6 @@ LARGE_FONT = ("Times", 12)
 NORMAL_FONT = ("Times", 10)
 SMALL_FONT = ("Times", 8)
 
-
 class MusSugWin(Tk):
      """
      Main Window for testing and then program
@@ -34,9 +33,9 @@ class MusSugWin(Tk):
      def __init__(self, *args, **kwargs):
 
           Tk.__init__(self, *args, **kwargs)
-          self.geometry('1000x1200')
+          self.geometry('1600x1800')
           self.title('Music Suggestions GUI Test')
-          self.iconbitmap('@/home/ntdgn/WIP-Programming.d/python.d/py-blue-trans-out.xbm')
+          self.iconbitmap('@./py-blue-trans-out.xbm')
           container = Frame(self)
 
           container.pack(side="top", fill="both", expand=True)
@@ -46,7 +45,7 @@ class MusSugWin(Tk):
 
           self.frames = {}
 
-          for frehme in (StartPage, PageOne, PageTwo, PageThree, PageFour):
+          for frehme in (StartPage, PageOne, PageTwo, PageThree, PageFour ):
                frame = frehme(container, self)
                self.frames[frehme] = frame
                frame.grid(row=0, column=0, sticky="nsew")
@@ -68,17 +67,16 @@ class StartPage(Frame):
           label = Label(self, text=GREETINGS, font=LARGE_FONT)
           label.pack(pady=10, padx=10)
 
-          button = Button(self, text="Visit Page 1", command=lambda: controller.show_frame(PageOne))
+          button = Button(self, text="Start", command=lambda: controller.show_frame(PageOne))
           button.pack()
 
-          button2 = Button(self, text="Visit Page 2", command=lambda: controller.show_frame(PageTwo))
+          button2 = Button(self, text="Quit", command=lambda: Frame.quit(self))
           button2.pack()
 
 
 class PageOne(Frame):
      """
      First page of gathering information
-     The inputs are currently just text entries for printing or file headers
      """
 
      def __init__(self, parent, controller):
@@ -117,29 +115,26 @@ class PageOne(Frame):
           #TODO: need more code here?
 
           label = Label(self, text="                        ", font=LARGE_FONT).pack(pady=10, padx=10)
-
           button1 = Button(self, text="Back to Home", command=lambda: controller.show_frame(StartPage))
           button1.pack()
-
-          button2 = Button(self, text="Page Two", command=lambda: controller.show_frame(PageTwo))
+          button2 = Button(self, text="Next", command=lambda: controller.show_frame(PageTwo))
           button2.pack()
+
 
 #          return(parnm, lityr, litevt)
 
 class PageTwo(Frame):
      """
      Text box entries for Psalms and Songs
-     Need to determine if ask the user to do a bit of work above pasting in the text.  Faster
-     parsing later if user adds a '|' or other mark to split title from rest of line.
      """
      def __init__(self, parent, controller):
           Frame.__init__(self, parent)
           label = Label(self, text="Suggestions from NPM", font=LARGE_FONT).pack(pady=10, padx=10)
           label = Label(self, text="Psalm Suggestions Here", font=LARGE_FONT).pack(pady=10, padx=10)
-          self.psalm_sugs = ScrolledText(parent=self, text='Psalm Suggestions entered here')
+          self.psalm_sugs = ScrolledTextV(rows=10, parent=self, text='Psalm Suggestions entered here, vertical bar after title')
 #          label = Label(self, text="                        ", font=LARGE_FONT).pack(pady=10, padx=10)
           label = Label(self, text="Song Suggestions Here", font=LARGE_FONT).pack(pady=10, padx=10)
-          self.song_sugs = ScrolledText(parent=self, text='Song Suggestions entered here')
+          self.song_sugs = ScrolledTextV(rows=40, parent=self, text='Song Suggestions entered here, put vertical bar after title')
 
           button1 = Button(self, text="Back to Home", command=lambda: controller.show_frame(StartPage))
           button1.pack()
@@ -147,90 +142,70 @@ class PageTwo(Frame):
           button2 = Button(self, text="Initial Entry", command=lambda: controller.show_frame(PageOne))
           button2.pack()
 
+          button3 = Button(self, text="Next", command=lambda: controller.show_frame(PageThree))
+          button3.pack()
+
 
 class PageThree(Frame):
      """
      This page just show progress for all the various other tasks that need to be done before
           proceding on to the the final page.  A bit crude code but slowly learning
           Tasks:
-               Input Search and Print for both songbooks into dictionaries
+               Input Search and Print for Both Songbooks
                parse psalms into known and unknown with known into print formatted and unknown raw
                parse songs into known and unknown similar to psalms
-                    Parse Algorithm(?):
-                         psalms-songs in text w/ w/o title identifier so first identify title
-                         if title known then format into formatted area
-                         else place raw line into unformatted text
-                    psalms are in PageTwo.psalms_sugs  songs are in PageTwo.song_sugs
      """
      def __init__(self, parent, controller):
           Frame.__init__(self, parent)
           label = Label(self, text="Processing Suggestions", font=LARGE_FONT).pack(pady=10, padx=10)
           label = Label(self, text="Inputting information from files", font=LARGE_FONT).pack(pady=10, padx=10)
-          self.gcprint = self.infyle_print('.gcprint.txt')
-          self.g3print = self.infyle_print('.g3print.txt')
-          self.gcsearch = self.infyle_search('.gcsearch.txt')
-          self.g3search = self.infyle_search('.g3search.txt')
+          '''   gcprint = infyle_print('.gcprint.txt.txt')
+          g3print = infyle_print('.g3print.txt')
+          gcsearch = infyle_search('.gcsearch.txt')
+          g3search = infile_search('.g3search.txt')
           #FIXME: Input from files Songbook Print and Search files - is this code correct
-
+          '''
           label = Label(self, text="Songbook Information read, begin parsing", font=LARGE_FONT).pack(pady=10, padx=10)
-          #TODO: Parse the psalm suggestions, note there may not be psalms suggestions some weeks
-          # psalm suggestions are in PageTwo.psalm_sugs
-          # print format == '/tG?# nnnn\tSongTitle\n'   TabGC# nnnTabSongTitle
-          #  no spaces after tabs
-          #GC first then G3
-
-          #TODO: Parse Song Suggestions
-          # same as psalms but for songs
+          #TODO: Parse the psalms then song suggestions
 
           label = Label(self, text="Ready to display", font=LARGE_FONT).pack(pady=10, padx=10)
           #FIXME: add command to go to next page here?
 
-          button1 = Button(self, text="Halt", command=lambda: controller.show_frame(PageTwo))
+          button1 = Button(self, text="Back", command=lambda: controller.show_frame(PageTwo))
           button1.pack()
 
           button2 = Button(self, text="Next", command=lambda: controller.show_frame(PageFour))
           button2.pack()
-
+'''
 #----------------------------------------------------------------------------------------------
      #entry for search a dictionary with title as key
-     #XXX: Ask user to put in '|' in page 2 or try to figure out title by regex or loop through
-     #     all till find a match or end.
      #entry for print a dictionary with inum as key
-     def infyle_search(self, infyle):
-          answer = {}
-          for line in infyle:
-               title, dummy_num, rest = line.split("|", 2)
-               snum = rest.split("|")
+     def infyle_search(infile,?) #FIXME: need to validate this  do I need to return entry
+          for line in infile:
+               title, num, snums = line.split("|") #FIXME: Need to check with Mike what happens when more than 1 song
 
-               title = title.strip()
-
-               answer[title] = snum
-
-          return answer
-
-     def infyle_print(self, infyle):
-          answer = {}
-          for line in infyle:
+          num = num.strip()
+          inum = int(num)
+          title = title.strip()
+          #TODO need to add code for 1 to n songs returned
+          
+     def infyle_print(infile,?) #FIXME: need to validate this  do I need to return entry
+          for line in infile:
                num, title = line.split("|")
-               num = num.strip()
-               inum = int(num)
-               title = title.strip()
-               if all([v == '0' for v in num]):
-                    continue
-               if all([v == '9' for v in num]) and title.startswith("End of Song"):
-                    continue
-               answer[inum] = title
 
-          return answer
-
+          num = num.strip()
+          inum = int(num)
+          title = title.strip()
+          if all([v == '0' for v in num]):
+               continue
+          if all([v == '9' for v in num]) and title.startswith("End of Song"):
+               continue
+          entry = (inum, title)
 
 
-     # TODO: Write next two stubs
-     def parse_it(self): # Don't know parameters yet
-        pass
-     def set_print(self):
-        pass
-
+     def parse_it(?)
+     def set_print()
+     '''
 
 
 
@@ -238,30 +213,30 @@ class PageFour(Frame):
      """
      Text box output for Psalms and Songs both known and unknown
      """
-
+     '''
      gc = "From Gather Comprehensive"
      g3 = "From Gather 3"
      ps = "Psalm Suggestions"
      ss = "Song Suggestions"
-
+     '''
      def __init__(self, parent, controller):
           Frame.__init__(self, parent)
           label = Label(self, text="Suggestions from NPM for ****", font=LARGE_FONT).pack(pady=10, padx=10)
           label = Label(self, text="Psalm Suggestions Here", font=LARGE_FONT).pack(pady=10, padx=10)
-#FIXME:          self.psalm_sugs = ScrolledText(parent=self, text='link to known Psalm Suggestions here ')
+          self.psalm_sugs = ScrolledTextV(rows=10, parent=self, text='Known Psalm Suggestions will be here ')
           label = Label(self, text="Unknown Psalm Suggestions Here", font=LARGE_FONT).pack(pady=10, padx=10)
-#FIXME          self.psalm_sugs = ScrolledText(parent=self, text='link to unknown psalm suggestions here')
+          self.psalm_sugs = ScrolledTextV(rows=2, parent=self, text='Unknown Psalm Suggestions here')
 
           label = Label(self, text="Song Suggestions Here", font=LARGE_FONT).pack(pady=10, padx=10)
-#FIXME:          self.song_sugs = ScrolledText(parent=self, text='Link to known Song Suggestions here')
+          self.song_sugs = ScrolledTextV(rows=20, parent=self, text='Link to known Song Suggestions here')
           label = Label(self, text="Unknown Song Suggestions Here", font=LARGE_FONT).pack(pady=10, padx=10)
-#FIXME:          self.song_sugs = ScrolledText(parent=self, text='Link to unknown Song Suggestions here')
+          self.song_sugs = ScrolledTextV(rows=2, parent=self, text='Link to unknown Song Suggestions here')
 
-
+          
           button1 = Button(self, text="Do Another", command=lambda: controller.show_frame(PageOne))
           button1.pack()
 
-          button2 = Button(self, text="Done", command=lambda: controller.show_frame(PageOne))
+          button2 = Button(self, text="Done", command=lambda: Frame.quit(self))
           button2.pack()
 
 
